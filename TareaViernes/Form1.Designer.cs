6043c7f75bc5dcb307f4bad47f1ac62636d6d474
Rodrigo Guzmán <ronckig1234@gmail.com>
2024-04-24 15:38:37 -0400

    Agregar archivos de proyecto.

---
+++ TareaViernes/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace TareaViernes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtVarUno = new System.Windows.Forms.TextBox();
+            this.txtVarDos = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TechCalc";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(304, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Suma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(538, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Resta\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(304, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(213, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Multiplicación";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(538, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(213, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "División";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(14, 322);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 73);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Salir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtVarUno
+            // 
+            this.txtVarUno.Location = new System.Drawing.Point(334, 96);
+            this.txtVarUno.Name = "txtVarUno";
+            this.txtVarUno.Size = new System.Drawing.Size(144, 20);
+            this.txtVarUno.TabIndex = 6;
+            this.txtVarUno.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtVarDos
+            // 
+            this.txtVarDos.Location = new System.Drawing.Point(573, 96);
+            this.txtVarDos.Name = "txtVarDos";
+            this.txtVarDos.Size = new System.Drawing.Size(144, 20);
+            this.txtVarDos.TabIndex = 7;
+            this.txtVarDos.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(637, 347);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(24, 24);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "X";
+            this.lblResultado.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(349, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "[Variable 1]";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(588, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "[Variable 2]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(504, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "[Resultado] :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(791, 409);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtVarDos);
+            this.Controls.Add(this.txtVarUno);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtVarUno;
+        private System.Windows.Forms.TextBox txtVarDos;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TareaViernes/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaViernes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string numero1 = txtVarUno.Text;
+            string numero2 = txtVarDos.Text;
+            int numero1_int = 0;
+            int numero2_int = 0;
+
+            if (numero1.Length > 0 && numero2.Length > 0)
+            {
+                try
+                {
+                    numero1_int = (int)Convert.ToDouble(numero1);
+                    numero2_int = Convert.ToInt32(numero2);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    txtVarUno.Text = "";
+                    txtVarDos.Text = "";
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Por favor, agrege una variable");
+            }
+
+            int resultado = numero1_int + numero2_int;
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string numero1 = txtVarUno.Text;
+            string numero2 = txtVarDos.Text;
+            int numero1_int = 0;
+            int numero2_int = 0;
+
+            if (numero1.Length > 0 && numero2.Length > 0)
+            {
+                try
+                {
+                    numero1_int = (int)Convert.ToDouble(numero1);
+                    numero2_int = Convert.ToInt32(numero2);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    txtVarUno.Text = "";
+                    txtVarDos.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, agrege una variable");
+            }
+
+            int resultado = numero1_int - numero2_int;
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string numero1 = txtVarUno.Text;
+            string numero2 = txtVarDos.Text;
+            int numero1_int = 0;
+            int numero2_int = 0;
+
+            if (numero1.Length > 0 && numero2.Length > 0)
+            {
+                try
+                {
+                    numero1_int = (int)Convert.ToDouble(numero1);
+                    numero2_int = Convert.ToInt32(numero2);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    txtVarUno.Text = "";
+                    txtVarDos.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, agrege una variable");
+            }
+
+            int resultado = numero1_int * numero2_int;
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string numero1 = txtVarUno.Text;
+            string numero2 = txtVarDos.Text;
+            int numero1_int = 0;
+            int numero2_int = 0;
+            double numero1_double = 0;
+
+            if(numero2 != "0")
+            {
+                if (numero1.Length > 0 && numero2.Length > 0)
+                {
+                    try
+                    {
+                        numero1_int = Convert.ToInt32(numero1);
+                        numero1_double = Convert.ToDouble(numero1_int);
+                        numero2_int = Convert.ToInt32(numero2);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        txtVarUno.Text = "";
+                        txtVarDos.Text = "";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, agrege una variable");
+                }
+                double resultado = numero1_double / numero2_int;
+                lblResultado.Text = resultado.ToString("0.####");
+            } else
+            {
+                MessageBox.Show("Error detectado: No es posible llevar dividir por 0");
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
